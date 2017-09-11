6da735cf90d047cafc7cb57c0ea6bd2932a94c66
Peter Strawn <pstrawn@stfrancishs.org>
2017-09-11 22:31:24 +0000

    Fix comments on var-4

--- chapter-1/var-4.c
+++ chapter-1/var-4.c
@@ -5,9 +5,9 @@ int main(void)
 {
     // examples of data types
     char strikeout = 'K';                   // 1 byte of memory for a character
-    string className = "This is CS50.";     // 4 or 8 bytes of memory for text
-    int age = 30;                           // 4 bytes of memory for an int
-    long long ageOfEarth = 4500000000;      // 8 bytes of memory for an int
+    string className = "This is CS50.";     // 4 or 8 bytes of memory for text (string of characters)
+    int age = 30;                           // 4 bytes of memory for an integer
+    long long ageOfEarth = 4500000000;      // 8 bytes of memory for an intger
     float pi = 3.14159265358979323846;      // 4 bytes of memory for a real number
     double morePi = 3.14159265358979323846; // 8 bytes of memory for a real number
     bool fun = true;                        // 1 byte of memory for true/false
